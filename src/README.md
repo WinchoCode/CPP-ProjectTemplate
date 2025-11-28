22c76bf82430168832aaf007a480f8f377a65c02
Edwin Avila <238208705+WinchoCode@users.noreply.github.com>
2025-11-28 17:30:58 +0100

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,3 @@
+### CPP-ProjectTemplate
+
+This project contains a template that can be used to start C++ projects from scratch.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, C++ Template World!" << endl;
+    return 0;
+}
